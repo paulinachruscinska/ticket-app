1803bbc8a23c86745768914140188549659aa988
Your Name <you@example.com>
2022-10-28 09:34:52 +0200

    Update cities

--- src/buy_ticket_js/buyTicketFirstPage.js
+++ src/buy_ticket_js/buyTicketFirstPage.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../landing_page_js/header";
 import Footer from "../landing_page_js/footer";
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 
 export default function BuyTicket1({cities, addCity, allCity}){
-    const selectCity = (e) =>{
-        e.preventDefault();
-        if(typeof addCity === 'function'){
-            addCity(e.currentTarget.id);
-        } else {
-            throw new Error('to nie jest funkcja')
-        }
-        console.log(allCity)
-
-    }
-
+    const [chooseCity, setChooseCity] = useState('blabla')
+    let navigate = useNavigate();
     return(
         <>
             <Header/>
             <section className='section-choose-city'>
                 <h1>Wybierz miasto</h1>
-                <div className='cities'>
+                <div className='cities' >
                     {cities.map(function(city){
                         return(
-                            <div onClick={selectCity} className='city' key={city.id} id={city.city}>
-                                <Link to={city.link}>
+                            <div
+                                onClick={()=>{
+                                    setChooseCity(city.city); // czemu ta funkcja nie działa?
+                                    if(typeof addCity === 'function'){
+                                        addCity(chooseCity);
+                                        navigate(city.link);
+                                    } else {
+                                        throw new Error('to nie jest funkcja')
+                                    }
+                                }}
+                                className='city' key={city.id}
+                            >
                                     <img src={city.img} alt='logo-city'/>
                                     <p className='city-name'>{city.city}</p>
-                                </Link>
                             </div>
                         )
                     })}
