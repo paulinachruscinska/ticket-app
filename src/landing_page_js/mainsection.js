8a570e6db1c730a5288519b6a97246c2a88c7fbe
Your Name <you@example.com>
2022-11-01 12:12:40 +0100

    Update branch

--- src/landing_page_js/mainsection.js
+++ src/landing_page_js/mainsection.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-
-
 export default function MainSection(){
     return(
         <Carousel autoPlay interval='3000' infiniteLoop showStatus={false} showThumbs={false}>
